ed83ebdfd0a81ac39682e73771ba4e4de1d74a65
miura39 <miurast.miura.y@gmail.com>
2019-12-14 15:53:54 +0900

    フロント実装修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   	
-  get  'messages/index'
-
+  resources :messages, only:[:index]
   root "messages#index"
 
-  resources :posts
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
