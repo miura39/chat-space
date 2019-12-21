fb926ceb99730075f2c19a800623d9ba9da3ef86
miura39 <miurast.miura.y@gmail.com>
2019-12-21 20:14:09 +0900

    メッセージ機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   
   devise_for :users
   root 'groups#index'
-  resources :users, only: [:index, :edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update]do
+  resources :users, only: [ :edit, :update]
+  resources :groups, only: [:new, :create, :update ,:edit]do
     resources :messages, only: [:index, :create]
   end
 end
